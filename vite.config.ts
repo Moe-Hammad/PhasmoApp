71a7d5874c9d2c8c790940ec7dc91aadb97745e4
Bemast3r <dahempvp@gmail.com>
2025-08-23 00:39:16 +0200

    improvement in DX with commands such as npm run dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  build: { outDir: "dist-react" },
+  build: {
+    outDir: "dist-react",
+  },
+  server: {
+    port: 5123,
+    strictPort: true,
+  },
 });
